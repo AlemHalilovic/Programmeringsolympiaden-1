55f378f3c9bf0fad11869b9eaddde1dd3016e3ae
Caspian2 <89245401+Caspian2@users.noreply.github.com>
2022-11-25 09:20:35 +0100

    Add project files.

---
+++ Programmeringsolympiaden 1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Uppgift_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Morötter = 40;
+            Console.WriteLine("Skriv in Torstid");
+            string Torstid = Console.ReadLine();
+            Console.WriteLine("Skriv in antal morötter tor har ätit");
+            string AntalMT = Console.ReadLine();
+            int TorsNyaTid = int.Parse(Console.ReadLine());
+            int TorsNyaAntal = int.Parse(Console.ReadLine());
+            Console.WriteLine(TorsNyaAntal / TorsNyaTid);
+
+
+
+        }
+    }
+}
